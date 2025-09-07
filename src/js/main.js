63602e5370d22382b49a58ab9845d1dcc0345a82
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-09-07 16:47:34 +0200

    change path image

--- src/js/main.js
+++ src/js/main.js
@@ -103,7 +103,7 @@ const renderSelectProduct = () => {
     deleteIcon.classList.add("remove-to-select");
     deleteIcon.title = "Eliminar";   
     const deleteImg = document.createElement("img");
-    deleteImg.src="public/images/recycle.png";
+    deleteImg.src="images/recycle.png";
     deleteImg.classList.add("delete-icon-img");
     deleteIcon.appendChild(deleteImg);
     deleteIcon.title = "Eliminar del carrito";
